c014ec8835d3f8eb3b0fe0e7462d1a68517de31f
Hamza Zafar <hamzafer3@gmail.com>
2023-11-26 14:55:38 -0500

    improvements

--- main/cmy2xyz/polynomial_regression.py
+++ main/cmy2xyz/polynomial_regression.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures
 
 from input.input import get_dataset
 from utils.save_results import save_results_to_excel
