8b68cf3ce36cafa5ca2511192158225c0e3b52e3
Hamza Zafar <hamzafer3@gmail.com>
2023-11-29 19:15:55 -0500

    implemented all the algorithms

--- main/cmy2xyz/main.py
+++ main/cmy2xyz/main.py
@@ -1,11 +1,40 @@
+from nn_Bayesian import process as nn_Bayesian
+from nn_DecisionTree import process as nn_DecisionTree
+from nn_DeepLearning import process as nn_DeepLearning
+from nn_Elastic import process as nn_Elastic
+from nn_GradientBoost import process as nn_GradientBoost
+from nn_Lasso import process as nn_Lasso
+from nn_PCR import process as nn_PCR
+from nn_PLSRegression import process as nn_PLSRegression
 from nn_RandomForestRegressor import process as nn_RandomForestRegressor
+from nn_RidgeRegression import process as nn_RidgeRegression
+from nn_SVM import process as nn_SVM
+from nn_k_Nearest import process as nn_k_Nearest
 from nn_logistic_lbfgs import process as nn_logistic_lbfgs
 from polynomial_regression import process as polynomial_regression
 
-# Machine Learning Algorithms
-nn_RandomForestRegressor('PC10', 'CMY', 'XYZ', visualize=False)
-nn_logistic_lbfgs('PC10', 'CMY', 'XYZ', visualize=False)
+# Constants
+DATASET_NAME = 'PC10'
+INPUT_TYPE = 'CMY'
+OUTPUT_TYPE = 'XYZ'
 
-# Polynomial Regression
+# Machine Learning Algorithms with PC10 dataset
+nn_RandomForestRegressor(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, visualize=False)
+nn_logistic_lbfgs(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, visualize=False)
+nn_Bayesian(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, visualize=False)
+nn_DecisionTree(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, visualize=False)
+nn_DeepLearning(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, visualize=False)
+nn_Elastic(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, visualize=False)
+nn_k_Nearest(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, visualize=False)
+nn_Lasso(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, visualize=False)
+nn_PCR(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, visualize=False)
+nn_PLSRegression(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, visualize=False)
+nn_RidgeRegression(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, visualize=False)
+
+# MultiOutputRegressor Algorithms with PC10 dataset
+nn_GradientBoost(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, visualize=False)
+nn_SVM(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, visualize=False)
+
+# Polynomial Regression with PC10 dataset
 for degree in range(1, 11):
-    polynomial_regression('PC10', 'CMY', 'XYZ', degree, visualize=False)
+    polynomial_regression(DATASET_NAME, INPUT_TYPE, OUTPUT_TYPE, degree, visualize=False)
---
+++ main/cmy2xyz/nn_Lasso.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+from input.input import get_dataset
+from utils.save_results import save_results_to_CSV
+from utils.xyz2lab import xyz2lab
+from visual.vis_lab import visualize_lab_values
+
+
+def process(dataset_name, input_type, output_type, visualize=False):
+    results = pd.DataFrame(columns=['Configuration', 'Mean Error', 'Median Error', 'Max Error'])
+
+    input_data = get_dataset(dataset_name, input_type)
+    output_data = get_dataset(dataset_name, output_type)
+
+    # Normalize the data to the range [0, 1]
+    scaler = MinMaxScaler()
+    input_cmy_norm = scaler.fit_transform(input_data)
+    output_xyz_norm = scaler.fit_transform(output_data)
+
+    # Split the dataset into training and testing sets (90% train, 10% test)
+    input_train, input_test, output_train, output_test = train_test_split(input_cmy_norm, output_xyz_norm,
+                                                                          test_size=0.1,
+                                                                          random_state=42)
+
+    # Lasso Regression configurations
+    configurations = [{'model': Lasso, 'alpha': 1.0, 'random_state': 42},
+                      {'model': Lasso, 'alpha': 0.5, 'random_state': 42}, ]
+
+    # Train and evaluate each configuration, which now includes Random Forest
+    for index, config in enumerate(configurations):
+        model = Lasso(alpha=config['alpha'], random_state=config['random_state'])
+
+        # Train the model
+        model.fit(input_train, output_train)
+
+        # Predict the output from the test input
+        output_pred = model.predict(input_test)
+
+        # Convert predicted XYZ to LAB
+        output_pred_lab = xyz2lab(output_pred)  # Replace with your actual conversion after denormalization
+
+        # Convert true XYZ to LAB for the test set
+        output_test_lab = xyz2lab(output_test)
+
+        # Calculate the Euclidean distance (error) between the predicted and true LAB values
+        errors = np.sqrt(np.sum((output_pred_lab - output_test_lab) ** 2, axis=1))
+
+        # Output the mean Euclidean error
+        mean_error = np.mean(errors)
+        median_error = np.median(errors)
+        max_error = np.max(errors)
+
+        # Add results to DataFrame
+        results.loc[index] = [str(config), mean_error, median_error, max_error]
+
+        if visualize:
+            # Visualize the predicted vs true LAB values
+            visualize_lab_values(output_test_lab, output_pred_lab)
+
+    # Identify the best configuration based on the lowest mean error
+    best_result = results.loc[results['Mean Error'].idxmin()]
+
+    # Add the best configuration as a new row or a separate section in the DataFrame
+    best_result_df = pd.DataFrame([best_result], index=['Best Configuration'])
+    final_results = pd.concat([results, best_result_df])
+
+    # Print all results and the best configuration
+    print("All Results:")
+    print(final_results)
+
+    # Use the updated function to save the results, now including the best configuration
+    csv_file_path = save_results_to_CSV(final_results, dataset_name, script_name=__file__)
+
+    print(f"Results saved to '{csv_file_path}'")
